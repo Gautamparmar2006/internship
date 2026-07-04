5433fb7bf17cc48c53fbb73a6503fd951fd6b2b5
Gautamparmar2006 <230130107074@gecg28.ac.in>
2026-07-04 10:33:01 +0530

    simple 10 line code of python

--- 10line.py
+++ 10line.py
@@ -5,5 +5,6 @@ print("Multiplication Table:")
 for i in range(1, 11):
     result = num * i
     print(f"{num} x {i} = {result}")
+print("Done!")
 print("Thank you,", name + "!")
 print("Program ended.")
